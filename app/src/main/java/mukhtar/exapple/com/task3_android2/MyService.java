e9ebdf5f57b390d7f2edb17d78a9190ad0ac8953
Mukhtar Bimurat <mukhtar.bimurat@ce.sdu.edu.kz>
2017-02-28 18:34:57 +0600

    hello world

---
+++ app/src/main/java/mukhtar/exapple/com/task3_android2/MyService.java
@@ -0,0 +1,16 @@
+package mukhtar.exapple.com.task3_android2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
