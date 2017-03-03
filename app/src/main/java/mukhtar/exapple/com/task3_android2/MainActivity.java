c2eed71ea2407d941d7d7262f33e7269a47d1b35
Mukhtar Bimurat <mukhtar.bimurat@ce.sdu.edu.kz>
2017-03-03 22:20:12 +0600

    code onsha emes

--- app/src/main/java/mukhtar/exapple/com/task3_android2/MainActivity.java
+++ app/src/main/java/mukhtar/exapple/com/task3_android2/MainActivity.java
@@ -1,14 +1,92 @@
 package mukhtar.exapple.com.task3_android2;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int TASK_CODE = 1;
+    public final static int STATUS_MESSAGE = 2;
+    public final static String PARAM_MESSAGE = "message";
+    public final static String PARAM_PINTENT = "pendingIntent";
+    public final static String BACK_MESSAGE = "pendingIntent";
+
+
+    EditText editText;
+    LinearLayout ll;
+    String LOG_TAG = "myLogs";
+    String currentMessage = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        ll = (LinearLayout)findViewById(R.id.linLayoutForMessage);
+    }
 
+    public void send(View v){
+        currentMessage = editText.getText().toString();
+        if(!currentMessage.equals("")){
+            PendingIntent pi = createPendingResult(TASK_CODE, new Intent(),0);
+            Intent intent = new Intent(this, MyService.class)
+                    .putExtra(PARAM_MESSAGE,currentMessage)
+                    .putExtra(PARAM_PINTENT,pi);
+            setMessage(currentMessage, false);
+            startService(intent);
+        }
+
+    }
+
+
+    public void setMessage(String message, boolean isBot){
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText(message);
+        tv.setTextSize(20);
+        if(isBot){
+            tv.setBackgroundColor(Color.YELLOW);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        }
+        else {
+            tv.setBackgroundColor(Color.GREEN);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        }
+
+        relativeLayout.addView(tv,layoutParams);
+        ll.addView(relativeLayout);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(LOG_TAG, "requestCode = " + requestCode + ", resultCode = "
+                + resultCode);
+        if(requestCode==TASK_CODE){
+            if(resultCode==STATUS_MESSAGE){
+                setMessage(data.getStringExtra(BACK_MESSAGE),true);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this,MyService.class));
     }
 }
--- app/src/main/java/mukhtar/exapple/com/task3_android2/MyService.java
+++ app/src/main/java/mukhtar/exapple/com/task3_android2/MyService.java
@@ -1,16 +1,131 @@
 package mukhtar.exapple.com.task3_android2;
 
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import android.app.NotificationManager;
+
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 public class MyService extends Service {
-    public MyService() {
+
+    final String LOG_TAG = "myLogs";
+    ExecutorService es;
+    boolean isConversation = false;
+    static int answer_id = 0;
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "MyService onCreate");
+        es = Executors.newFixedThreadPool(1);
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MyService onDestroy");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "MyService onStartCommand");
+
+        String message = intent.getStringExtra(MainActivity.PARAM_MESSAGE);
+        PendingIntent pi = intent.getParcelableExtra(MainActivity.PARAM_PINTENT);
+        double time = 0.1;
+
+        if(message.equals("hi")&&!isConversation){
+            isConversation = true;
+        }
+
+        if(isConversation){
+            time = ((new Random()).nextInt(15)+5);
+            MyRun mr = new MyRun(time, startId, pi, message);
+            es.execute(mr);
+        }
+
+        if(message.equals("bye")&&isConversation){
+            isConversation = false;
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    void sendNotif(String message) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                        .setContentTitle("My notification")
+                        .setContentText("bot has answered: \n"+message);
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(MyService.answer_id++, mBuilder.build());
+    }
+
+    class MyRun implements Runnable {
+
+        double time;
+        int startId;
+        String message;
+        PendingIntent pi;
+
+        public MyRun(double time, int startId, PendingIntent pi, String message) {
+            this.message = message;
+            this.time = time;
+            this.startId = startId;
+            this.pi = pi;
+            Log.d(LOG_TAG, "MyRun#" + startId + " create");
+        }
+
+        public void run() {
+
+            String answer;
+            if(message.equals("hi")){ answer="hi"; time = 0.1;}
+            else if(message.equals("bye")){ answer="bye"; time = 0.1;}
+            else{
+                answer = "random number " + ((new Random()).nextInt(60)+10);
+            }
+            try {
+                Thread.sleep((long)(time*1000));
+                sendNotif(answer);
+                pi.send( MyService.this, MainActivity.STATUS_MESSAGE, new Intent().
+                        putExtra(MainActivity.BACK_MESSAGE, answer));
+            } catch (CanceledException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
